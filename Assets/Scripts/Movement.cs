0afad398505353c0ddc8888af7ea4404ed8d7f19
AndersenCastaneda <34457834+AndersenCastaneda@users.noreply.github.com>
2020-12-22 13:27:30 -0500

    Add task 3

---
+++ Assets/Scripts/Movement.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+
+public class Movement
+{
+    private readonly Rigidbody _rb;
+    private readonly PlayerInput _input;
+    private readonly float _yPos = 1.2f;
+
+    public Movement(Rigidbody rigidbody, PlayerInput playerInput)
+    {
+        _rb = rigidbody;
+        _input = playerInput;
+    }
+
+    public void Tick(float speed, float time)
+    {
+        _rb.AddForce(new Vector3(_input.Horizontal, _yPos, _input.Vertical) * speed * time);
+    }
+}
---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+
+[RequireComponent(typeof(Rigidbody))]
+public class PlayerController : MonoBehaviour
+{
+    public float speed;
+    private Rigidbody _rigidbody;
+    private PlayerInput _input;
+    private Movement _movement;
+
+    private void Awake() => _rigidbody = GetComponent<Rigidbody>();
+
+    private void Start()
+    {
+        _input = new PlayerInput();
+        _movement = new Movement(_rigidbody, _input);
+    }
+
+    private void Update() => _input.ReadInput();
+
+    private void FixedUpdate() => _movement.Tick(speed, Time.fixedDeltaTime);
+}
