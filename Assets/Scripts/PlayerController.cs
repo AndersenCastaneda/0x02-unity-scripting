a259058bfb15aa4486955d5114ef78c9d2d6cd16
AndersenCastaneda <34457834+AndersenCastaneda@users.noreply.github.com>
2020-12-22 14:18:47 -0500

    Add task 5

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -7,11 +7,13 @@ public class PlayerController : MonoBehaviour
     private Rigidbody _rigidbody;
     private PlayerInput _input;
     private Movement _movement;
+    private int score = 0;
 
     private void Awake() => _rigidbody = GetComponent<Rigidbody>();
 
     private void Start()
     {
+        _rigidbody.constraints = RigidbodyConstraints.FreezePositionY;
         _input = new PlayerInput();
         _movement = new Movement(_rigidbody, _input);
     }
@@ -19,4 +21,13 @@ public class PlayerController : MonoBehaviour
     private void Update() => _input.ReadInput();
 
     private void FixedUpdate() => _movement.Tick(speed, Time.fixedDeltaTime);
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.CompareTag("Pickup"))
+        {
+            Debug.Log($"Score {++score}");
+            other.GetComponent<ICollectable>().Collect();
+        }
+    }
 }
